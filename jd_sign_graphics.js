13d2291979d5c2f06b257aafbc52a011f27ad4db
DzPian <490857007@qq.com>
2021-12-17 19:35:36 +0800

    京东图形签到

--- jd_sign_graphics.js
+++ jd_sign_graphics.js
@@ -1,16 +1,10 @@
 /* 
 cron 14 10 * * * https://raw.githubusercontent.com/smiek2121/scripts/master/jd_sign_graphics.js
-只支持nodejs环境
-需要安装依赖 
-npm i png-js 或者 npm i png-js -S
 
-如果 read ECONNRESET 错误 可以试试
-环境变量 JOY_HOST
-修改域名 https://jdjoy.jd.com 可以改成ip https://49.7.27.236
 */
 
-const validator = require('./JDJRValidator_Pure.js');
-const Faker=require('./sign_graphics_validate.js') 
+// const validator = require('./JDJRValidator_Pure.js');
+// const Faker=require('./sign_graphics_validate.js');
 
 const $ = new Env('京东签到图形验证');
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -20,18 +14,9 @@ CryptoScripts()
 $.CryptoJS = $.isNode() ? require('crypto-js') : CryptoJS;
 let cookiesArr = [], cookie = '';
 
-if ($.isNode()) {
-  Object.keys(jdCookieNode).forEach((item) => {
-    cookiesArr.push(jdCookieNode[item])
-  })
-  if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => { };
-} else {
-  cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
-}
 let message = '', subTitle = '', beanNum = 0;
 let fp = ''
 let eid = ''
-let UA = ""
 let signFlag = false
 let successNum = 0
 let errorNum = 0
@@ -39,22 +24,31 @@ let JD_API_HOST = 'https://jdjoy.jd.com'
 $.invokeKey = "q8DNJdpcfRQ69gIx"
 $.invokeKey = $.isNode() ? (process.env.JD_invokeKey ? process.env.JD_invokeKey : `${$.invokeKey}`) : ($.getdata('JD_invokeKey') ? $.getdata('JD_invokeKey') : `${$.invokeKey}`);
 let lkt = 0
-if(process.env.JOY_HOST){
-  JD_API_HOST = process.env.JOY_HOST
+
+if ($.isNode()) {
+  if(process.env.JOY_HOST){
+    JD_API_HOST = process.env.JOY_HOST
+  }
+  Object.keys(jdCookieNode).forEach((item) => {
+    cookiesArr.push(jdCookieNode[item])
+  })
+  if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => { };
+} else {
+  cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
-getUA()
-$.get = validator.injectToRequest($.get.bind($), 'channelSign', $.UA)
-$.post = validator.injectToRequest($.post.bind($), 'channelSign', $.UA)
+// getUA()
+// $.get = validator.injectToRequest($.get.bind($), 'channelSign', $.UA)
+// $.post = validator.injectToRequest($.post.bind($), 'channelSign', $.UA)
 
 const turnTableId = [
-  { "name": "PLUS会员定制", "id": 1265, "url": "https://prodev.m.jd.com/mall/active/N9MpLQdxZgiczZaMx2SzmSfZSvF/index.html" },
-  { "name": "京东商城-内衣", "id": 1071, "url": "https://prodev.m.jd.com/mall/active/4PgpL1xqPSW1sVXCJ3xopDbB1f69/index.html" },
   { "name": "京东商城-健康", "id": 527, "url": "https://prodev.m.jd.com/mall/active/w2oeK5yLdHqHvwef7SMMy4PL8LF/index.html" },
   { "name": "京东商城-清洁", "id": 446, "url": "https://prodev.m.jd.com/mall/active/2Tjm6ay1ZbZ3v7UbriTj6kHy9dn6/index.html" },
   { "name": "京东商城-个护", "id": 336, "url": "https://prodev.m.jd.com/mall/active/2tZssTgnQsiUqhmg5ooLSHY9XSeN/index.html" },
-  { "name": "京东商城-童装", "id": 511, "url": "https://prodev.m.jd.com/mall/active/3Af6mZNcf5m795T8dtDVfDwWVNhJ/index.html" },
   { "name": "京东商城-母婴", "id": 458, "url": "https://prodev.m.jd.com/mall/active/3BbAVGQPDd6vTyHYjmAutXrKAos6/index.html" },
   { "name": "京东商城-数码", "id": 347, "url": "https://prodev.m.jd.com/mall/active/4SWjnZSCTHPYjE5T7j35rxxuMTb6/index.html" },
+  { "name": "PLUS会员定制", "id": 1265, "url": "https://prodev.m.jd.com/mall/active/N9MpLQdxZgiczZaMx2SzmSfZSvF/index.html" },
+  { "name": "京东商城-童装", "id": 511, "url": "https://prodev.m.jd.com/mall/active/3Af6mZNcf5m795T8dtDVfDwWVNhJ/index.html" },
+  { "name": "京东商城-内衣", "id": 1071, "url": "https://prodev.m.jd.com/mall/active/4PgpL1xqPSW1sVXCJ3xopDbB1f69/index.html" },
   { "name": "京东超市", "id": 1204, "url": "https://pro.m.jd.com/mall/active/QPwDgLSops2bcsYqQ57hENGrjgj/index.html" },
   { "name": "美妆-3", "id": 1082, "shopid": 1000004123, "url": "https://sendbeans.jd.com/jump/index/" },
 ]
@@ -64,7 +58,6 @@ const turnTableId = [
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
     return;
   }
-  $.appId = '9a4de';
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -79,13 +72,12 @@ const turnTableId = [
       subTitle = '';
       lkt = new Date().getTime()
       await getUA()
-      await requestAlgo();
       await signRun()
       const UTC8 = new Date().getTime() + new Date().getTimezoneOffset()*60000 + 28800000;
       $.beanSignTime = new Date(UTC8).toLocaleString('zh', {hour12: false}).replace(' 24:',' 00:');
       let msg = `【京东账号${$.index}】${$.nickName || $.UserName}\n【签到时间】:  ${$.beanSignTime}\n【签到概览】:  成功${successNum}个, 失败${errorNum}个${invalidNum && "，失效"+invalidNum+"个" || ""}\n${beanNum > 0 && "【签到奖励】:  "+beanNum+"京豆" || ""}\n`
       message += msg + '\n'
-      $.msg($.name, msg);
+      if($.isNode()) $.msg($.name, msg);
     }
   }
   await showMsg();
@@ -112,12 +104,16 @@ async function signRun() {
     }else{
       errorNum++;
     }
-    await $.wait(1000)
+    let time = Math.random() * 4000 + 16000
+    console.log(`等待${(time/1000).toFixed(3)}秒`)
+    await $.wait(parseInt(time, 10))
   }
 }
 
 function Login(i) {
-  return new Promise(resolve => {
+  return new Promise(async resolve => {
+    $.appId = '9a4de';
+    await requestAlgo();
     $.get(taskUrl(turnTableId[i].id), async (err, resp, data) => {
       try {
         if (err) {
@@ -130,10 +126,15 @@ function Login(i) {
             if (data.success && data.data) {
               data = data.data
               if (data.hasSign === false) {
-                let arr = await Faker.getBody(UA,turnTableId[i].url)
-                fp = arr.fp
-                await getEid(arr)
-                await Sign(i)
+                // let arr = await Faker.getBody($.UA,turnTableId[i].url)
+                // fp = arr.fp
+                // await getEid(arr)
+                $.appId = 'b342e';
+                await requestAlgo();
+                await Sign(i,1)
+                let time = Math.random() * 4000 + 16000
+                console.log(`等待${(time/1000).toFixed(3)}秒`)
+                await $.wait(parseInt(time, 10))
               } else if (data.hasSign === true) {
                 if(data.records && data.records[0]){
                   for(let i in data.records){
@@ -173,34 +174,38 @@ function Login(i) {
     })
   })
 }
-function Sign(i) {
+function Sign(i,t) {
   return new Promise(resolve => {
-    $.post(tasPostkUrl(turnTableId[i].id), (err, resp, data) => {
+    let options = tasPostkUrl(turnTableId[i].id)
+    $.post(options, async (err, resp, data) => {
       try {
         if (err) {
           console.log(`\n${turnTableId[i].name} 签到: API查询请求失败 ‼️‼️`)
           throw new Error(err);
         } else {
-          if (data) {
-            // console.log(data)
-            data = JSON.parse(data);
-            if (data.success && data.data) {
-              data = data.data
-              if (Number(data.jdBeanQuantity) > 0) beanNum += Number(data.jdBeanQuantity)
+          let res = $.toObj(data,data)
+          if (typeof res === 'object') {
+            if (res.success && res.data) {
+              let resData = res.data
+              if (Number(resData.jdBeanQuantity) > 0) beanNum += Number(resData.jdBeanQuantity)
               signFlag = true;
-              console.log(`${turnTableId[i].name} 签到成功:获得 ${Number(data.jdBeanQuantity)}京豆`)
+              console.log(`${turnTableId[i].name} 签到成功:获得 ${Number(resData.jdBeanQuantity)}京豆`)
             } else {
-              if (data.errorMessage) {
-                if(data.errorMessage.indexOf('已签到') > -1 || data.errorMessage.indexOf('今天已经签到') > -1){
+              if (res.errorMessage) {
+                if(res.errorMessage.indexOf('已签到') > -1 || res.errorMessage.indexOf('今天已经签到') > -1){
                   signFlag = true;
                 }
-                console.log(`${turnTableId[i].name} ${data.errorMessage}`)
+                if(res.errorMessage.indexOf('火爆') > -1 && t == 1){
+                  await Sign(i,2)
+                }else{
+                  console.log(`${turnTableId[i].name} ${res.errorMessage}`)
+                }
               } else {
-                console.log(`${turnTableId[i].name} ${JSON.stringify(data)}`)
+                console.log(`${turnTableId[i].name} ${data}`)
               }
             }
           } else {
-            console.log(`京豆api返回数据为空，请检查自身原因`)
+            console.log(`${turnTableId[i].name} ${data}`)
           }
         }
       } catch (e) {
@@ -246,9 +251,11 @@ function getEid(arr) {
 }
 
 function taskUrl(turnTableId) {
-  let body = {"turnTableId":`${turnTableId}`,"invokeKey":$.invokeKey}
-  let h5st = geth5st(body) || 'undefined'
-  const url = `https://api.m.jd.com/api?clientVersion=1.2.5&client=jxh5&appid=jdchoujiang_h5&t=${Date.now()}&functionId=turncardChannelDetail&body=${escape(JSON.stringify(body))}&h5st=${h5st}`
+  let time = Date.now()
+  let body = {"turnTableId":`${turnTableId}`}
+  let t = [{"key":"appid","value":"jdchoujiang_h5"},{"key":"body","value":$.CryptoJS.SHA256($.toStr(body,body)).toString()},{"key":"client","value":""},{"key":"clientVersion","value":""},{"key":"functionId","value":"turncardChannelDetail"},{"key":"t","value":time}]
+  let h5st = geth5st(t) || 'undefined'
+  let url = `https://api.m.jd.com/api?client=&clientVersion=&appid=jdchoujiang_h5&t=${time}&functionId=turncardChannelDetail&body=${escape(JSON.stringify(body))}&h5st=${h5st}`
   return {
     url,
     headers: {
@@ -264,17 +271,17 @@ function taskUrl(turnTableId) {
   }
 }
 function tasPostkUrl(turnTableId) {
-  let body = {"turnTableId":`${turnTableId}`,"fp":fp,"lks":$.CryptoJS.MD5(""+$.invokeKey+lkt).toString(),"lkt":lkt,"invokeKey":$.invokeKey}
-  let h5st = geth5st(body) || 'undefined'
-  const url = `https://api.m.jd.com/api?clientVersion=1.2.5&client=jxh5&appid=jdchoujiang_h5&t=${Date.now()}&functionId=turncardChannelSign&body=${JSON.stringify(body)}&h5st=${h5st}&turnTableId=${turnTableId}&fp=${fp}&eid=${eid}&lks=${$.CryptoJS.MD5(""+$.invokeKey+lkt).toString()}&lkt=${lkt}&invokeKey=${$.invokeKey}`
+  let time = Date.now()
+  let body = {"turnTableId":`${turnTableId}`,"fp":'',"eid":''}
+  let t = [{"key":"appid","value":"jdchoujiang_h5"},{"key":"body","value":$.CryptoJS.SHA256($.toStr(body,body)).toString()},{"key":"client","value":""},{"key":"clientVersion","value":""},{"key":"functionId","value":"turncardChannelSign"},{"key":"t","value":time}]
+  let h5st = geth5st(t) || 'undefined'
+  let url = `https://api.m.jd.com/api?client=&clientVersion=&appid=jdchoujiang_h5&functionId=turncardChannelSign&t=${time}&body=${escape(JSON.stringify(body))}&h5st=${h5st}`
   return {
     url,
     headers: {
       "Accept": "application/json, text/plain, */*",
       "Accept-Encoding": "gzip, deflate, br",
       "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-      "Connection": "keep-alive",
-      "Content-Type": "application/x-www-form-urlencoded",
       'Cookie': cookie,
       "Origin": "https://prodev.m.jd.com",
       "Referer": "https://prodev.m.jd.com/",
@@ -284,30 +291,25 @@ function tasPostkUrl(turnTableId) {
 }
 
 async function requestAlgo() {
-  $.fp = (getRandomIDPro({ size: 13 }) + Date.now()).slice(0, 16);
+  var s = "", a = "0123456789", u = a, c = (Math.random() * 10) | 0;
+  do{
+    ss = getRandomIDPro({ size: 1 ,customDict:a})+""
+    if(s.indexOf(ss) == -1) s += ss
+  }while (s.length < 3)
+  for(let i of s.slice()) u = u.replace(i,'')
+  $.fp = getRandomIDPro({size:c, customDict:u})+""+s+getRandomIDPro({size:(14-(c+3))+1, customDict:u})+c+""
   let opts = {
-    "url": `https://cactus.jd.com/request_algo?g_ty=ajax`,
+    url: `https://cactus.jd.com/request_algo?g_ty=ajax`,
     headers: {
-      'Authority': 'cactus.jd.com',
-      'Pragma': 'no-cache',
-      'Cache-Control': 'no-cache',
       'Accept': 'application/json',
       'Content-Type': 'application/json',
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
       'Origin': 'https://prodev.m.jd.com',
-      'Sec-Fetch-Site': 'cross-site',
+      'Referer': 'https://prodev.m.jd.com/',
       'User-Agent': $.UA,
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Dest': 'empty',
-      'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7'
     },
-    'body': JSON.stringify({
-      "version": "3.0",
-      "fp": $.fp,
-      "appId": $.appId,
-      "timestamp": Date.now(),
-      "platform": "web",
-      "expandParams": ""
-    })
+    body: `{"version":"3.0","fp":"${$.fp}","appId":"${$.appId}","timestamp":${Date.now()},"platform":"web","expandParams":""}`
   }
   return new Promise(async resolve => {
     $.post(opts, (err, resp, data) => {
@@ -347,19 +349,17 @@ function getRandomIDPro() {
   for (; a--;) i += e[(Math.random() * e.length) | 0];
   return i;
 }
-function geth5st(body){
-  let key = $.CryptoJS.SHA256(body).toString()
-  let time = Date.now()
-  const timestamp = format("yyyyMMddhhmmssSSS", time);
-  const hash1 = $.genKey($.token, $.fp.toString(), timestamp.toString(), $.appId.toString(), $.CryptoJS).toString($.CryptoJS.enc.Hex);
-  let t = [{"key":"appid","value":"u"},{"key":"body","value":key},{"key":"client","value":"apple"},{"key":"clientVersion","value":"8.3.6"},{"key":"functionId","value":"getCoupons"}]
+function geth5st(t){
+  // return ''
   let a = t.map(function(e) {
-  return e["key"] + ":" + e["value"]
+    return e["key"] + ":" + e["value"]
   })["join"]("&")
-  // s = $.CryptoJS.HmacSHA256(a,e).toString()
-  const hash2 = $.CryptoJS.HmacSHA256(a, hash1.toString()).toString($.CryptoJS.enc.Hex);
-  // h5st = `${ts};${fp};${ai};${tk};${s};3.0;${time}`
-  h5st = ["".concat(timestamp.toString()), "".concat($.fp.toString()), "".concat($.appId.toString()), "".concat($.token), "".concat(hash2),"3.0","".concat(time)].join(";")
+  let time = Date.now()
+  let hash1 = ''
+  let timestamp = format("yyyyMMddhhmmssSSS", time);
+  hash1 = $.genKey($.token, $.fp.toString(), timestamp.toString(), $.appId.toString(), $.CryptoJS).toString();
+  const hash2 = $.CryptoJS.HmacSHA256(a, hash1.toString()).toString();
+  let h5st = ["".concat(timestamp.toString()), "".concat($.fp.toString()), "".concat($.appId.toString()), "".concat($.token), "".concat(hash2),"3.0","".concat(time)].join(";")
   return encodeURIComponent(h5st)
 }
 function format(a, time) {
@@ -408,7 +408,7 @@ function jsonParse(str) {
 }
 
 function getUA(){
-  $.UA = `jdapp;iPhone;10.1.0;14.3;${randomString(40)};network/wifi;model/iPhone12,1;addressid/4199175193;appBuild/167774;jdSupportDarkMode/0;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1`
+  $.UA = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36`
 }
 function randomString(e) {
   e = e || 32;
