a5e2f99702f73074cb1fc434b53a1d04235ad4ee
DzPian <490857007@qq.com>
2022-01-17 23:16:05 +0800

    京东频道签到

--- jd_m_sign.js
+++ jd_m_sign.js
@@ -1,7 +1,7 @@
 
 /*
 京东通天塔--签到
-脚本更新时间：2022-1-5
+脚本更新时间：2021-12-17 14:20
 脚本兼容: Node.js
 ===========================
 [task_local]
@@ -9,7 +9,7 @@
 3 0 * * * jd_m_sign.js, tag=京东通天塔--签到, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
  */
 
-const $ = new Env('京东通天塔--签到');
+const $ = new Env('京东频道签到');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
@@ -70,10 +70,6 @@ async function jdsign() {
     await getInfo("https://prodev.m.jd.com/mall/active/3MFSkPGCDZrP2WPKBRZdiKm9AZ7D/index.html");//同城签到
     await $.wait(1000)
     await getInfo("https://pro.m.jd.com/mall/active/kPM3Xedz1PBiGQjY4ZYGmeVvrts/index.html");//陪伴
-    await $.wait(1000)
-    await getInfo("https://pro.m.jd.com/mall/active/3SC6rw5iBg66qrXPGmZMqFDwcyXi/index.html");//京东图书
-    await $.wait(1000)
-    await getInfo("https://prodev.m.jd.com/mall/active/whyxjHX23eNgT6Xvej19JnamfEH/index.html");//签到1.14结束
   } catch (e) {
     $.logErr(e)
   }
