d44a6c9a390b6cf6c13d90031694e26d970a18e4
DzPian <490857007@qq.com>
2022-06-12 20:07:10 +0800

    金榜创造营 6-12

--- jd_gold_creator.js
+++ jd_gold_creator.js
@@ -45,10 +45,6 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
     return;
   }
-  console.log('金榜创造营\n' +
-      '活动时间：2021-5-21至2021-12-31\n' +
-      '活动地址：https://h5.m.jd.com/babelDiy/Zeus/2H5Ng86mUJLXToEo57qWkJkjFPxw/index.html\n' +
-      '活动入口：京东app首页搜：玩一玩');
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -104,7 +100,7 @@ async function getDetail() {
   for (let item of $.subTitleInfos) {
     console.log(`\n开始给【${item['longTitle']}】主题下的商品进行投票`);
     await goldCreatorDetail(item['matGrpId'], item['subTitleId'], item['taskId'], item['batchId']);
-    await $.wait(2000);
+    await $.wait(4000);
   }
 }
 function goldCreatorTab() {
@@ -215,7 +211,7 @@ async function doTask2(batchId) {
         body['type'] = 2;
       }
       await goldCreatorDoTask(body);
-      await $.wait(2000);
+      await $.wait(4000);
     }
   }
   if ($.signTask['taskStatus'] === 1) {
